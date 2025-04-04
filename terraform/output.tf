e94037c638cf4c5569add8b0b0101e6eeaeaec76
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2025-04-05 00:20:31 +0100

    Edit: output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,8 @@
 output "reference_bucket" {
-  value = aws.aws_s3_bucket.reference_bucket.id
+  value = aws_s3_bucket.reference_bucket.id
 }
 
 output "database_bucket" {
-  value = aws.aws_s3_bucket.database_bucket.id
+  value = aws_s3_bucket.database_bucket.id
 }
 
