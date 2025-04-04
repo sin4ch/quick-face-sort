a1fb99c556ab27aefbd17b23f3d645983ceab4ab
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2025-04-05 00:33:16 +0100

    Add: s3 bucket ownership control to terraform for both database and reference buckets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,40 @@
 provider "aws" {}
 
+
+
+
+# Reference Bucket
 resource "aws_s3_bucket" "reference_bucket" {
-  bucket = "reference_bucket"
+  bucket = "quicksort-reference-bucket-20250404"
 }
 
+# Reference bucket ownership control
+resource "aws_s3_bucket_ownership_controls" "reference_bucket_ownership" {
+  bucket = aws_s3_bucket.reference_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Reference bucket acl. depending on ownership control
 resource "aws_s3_bucket_acl" "reference_bucket_acl" {
+    depends_on = [ aws_s3_bucket_ownership_controls.reference_bucket_ownership ]
     bucket = aws_s3_bucket.reference_bucket.id
     acl   = "private"
+}
+
+resource "aws_s3_bucket" "database_bucket" {
+  bucket = "quicksort-database-bucket-20250404"
+}
+
+resource "aws_s3_bucket_ownership_controls" "database_bucket_ownership" {
+  bucket = aws_s3_bucket.database_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "database_bucket_acl" {
     bucket = aws_s3_bucket.database_bucket.id
     acl   = "private"
-}
-
-resource "aws_s3_bucket" "database_bucket" {
-  bucket = "database_bucket"
 }
