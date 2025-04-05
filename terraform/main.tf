9df88fa6236e64ad9618e2a91fbbbdb43ba55ce8
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2025-04-05 01:06:16 +0100

    Add: module docs for reference and database bucket blocks in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,17 @@
 provider "aws" {}
 
-# Reference Bucket
+# ----------------------------------------------------
+# REFERENCE OBJECT BLOCK
+# It contains:
+# - The reference bucket declaration
+# - The ownership control for the bucket
+# - The bucket policy access block
+# ----------------------------------------------------
+
 resource "aws_s3_bucket" "reference_bucket" {
   bucket = "quicksort-reference-bucket-20250404"
 }
 
-# Reference bucket ownership control
 resource "aws_s3_bucket_ownership_controls" "reference_bucket_ownership" {
   bucket = aws_s3_bucket.reference_bucket.id
   rule {
@@ -13,16 +19,40 @@ resource "aws_s3_bucket_ownership_controls" "reference_bucket_ownership" {
   }
 }
 
-# Reference Bucket
+resource "aws_s3_bucket_public_access_block" "reference_access_block" {
+  bucket = aws_s3_bucket.reference_bucket.id
+  
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+ 
+# ----------------------------------------------------
+# DATABASE OBJECT BLOCK
+# It contains:
+# - The database bucket declaration
+# - The ownership control for the bucket
+# - The bucket policy access block
+# ----------------------------------------------------
+
 resource "aws_s3_bucket" "database_bucket" {
   bucket = "quicksort-database-bucket-20250404"
 }
 
-# Reference bucket ownership control
 resource "aws_s3_bucket_ownership_controls" "database_bucket_ownership" {
   bucket = aws_s3_bucket.database_bucket.id
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "database_access_block" {
+  bucket = aws_s3_bucket.database_bucket.id
+  
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
